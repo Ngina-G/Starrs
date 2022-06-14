e0bcc52bf0f74ebbd1601386cdb0be76986912db
Ngina <Nginagitau11@outlook.com>
2022-06-15 01:59:52 +0300

    update: adds serializera

---
+++ awwards/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import Profile,Post
+from django.contrib.auth.models import User
+
+class ProfileSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Profile
+        fields = ('email', 'name', 'bio','profile_image','location')
+        
+
+class PostSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Post
+        fields = ('id','title','description','url','image')
+        
+        
+class UserSerializer(serializers.ModelSerializer):
+    profile = ProfileSerializer(read_only=True)
+    posts = PostSerializer(many=True,read_only=True)   
+    
+    
+    class Meta:
+        model = User
+        fields = ('id','url','username','profile','posts')   
--- awwards/views.py
+++ awwards/views.py
@@ -1,24 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .forms import SignupForm,PostForm,UpdateUserForm,UpdateUserProfileForm,RatingForm
+from rest_framework import viewsets
+from .models import Profile,Post,Rating
+from .serializers import ProfileSerializer,UserSerializer,PostSerializer
+from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
+import random
+
 
 # Create your views here.
-def index(request):
-    # images = Post.objects.all()
-    # users = User.objects.all()
-    # if request.method == 'POST':
-    #     form = PostForm(request.POST, request.FILES)
-    #     if form.is_valid():
-    #         post = form.save(commit=False)
-    #         post.user = request.user.profile
-    #         post.save()
-    #         return HttpResponseRedirect(request.path_info)
-    # else:
-    #     form = PostForm()
-    #     comment_form = CommentForm()
-    params = {
-        # 'images': images,
-        # 'form': form,
-        # 'comment_form': comment_form,
-        # 'users': users,
 
-    }
-    return render(request, 'index.html', params)
+
+def  index(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False )
+            post.user = request.user
+            post.save()
+    else:
+        form = PostForm()      
+    try:
+        posts = Post.objects.all()
+        posts = posts[::-1]
+        a_post = random.randint(0, len(posts)-1)
+        random_post = posts[a_post]
+        print(random_post)
+    except Post.DoesNotExist:
+        posts=None
+
+    return render(request, 'index.html', {'posts': posts,'form': form,'random_post': random_post})
+
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer 
