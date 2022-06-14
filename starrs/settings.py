1e6baf5c8c26f5fa36f8243bf5a6ae31d9a2a5d5
Ngina <Nginagitau11@outlook.com>
2022-06-15 00:47:41 +0300

    update: creates database

--- starrs/settings.py
+++ starrs/settings.py
@@ -76,10 +76,12 @@ WSGI_APPLICATION = 'starrs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
+DATABASES={
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'starrs',
+        'USER': 'ngina',
+        'PASSWORD':'Postgres',
     }
 }
 
@@ -108,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
