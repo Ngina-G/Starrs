16c5b2bafd1532d31d16dd98becc9502afba6742
Ngina <Nginagitau11@outlook.com>
2022-06-15 01:08:21 +0300

    update: adds  url config

--- awwards/urls.py
+++ awwards/urls.py
@@ -6,13 +6,6 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     path('', views.index, name='home'),
-    path('profile/<user_name>/', views.profile, name='profile'),
-    path('user_profile/<username>/', views.user_profile, name='user_profile'),
-    path('post/<id>', views.post_comment, name='comment'),
-    path('like/<int:pk>', views.LikePost, name='like_image'),
-    path('search/', views.search_profile, name='search'),
-    path('signup/', views.signup, name ='signup'),
-    path('logout/',views.logout_user, name='logout'),
 
 ]
 if settings.DEBUG:
