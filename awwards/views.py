2212bc13beee1da4ced49a038fbfc5f3a496d609
Ngina <Nginagitau11@outlook.com>
2022-06-15 01:08:41 +0300

    update: adds first migration

--- awwards/views.py
+++ awwards/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    images = Post.objects.all()
+    users = User.objects.all()
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user.profile
+            post.save()
+            return HttpResponseRedirect(request.path_info)
+    else:
+        form = PostForm()
+        comment_form = CommentForm()
+    params = {
+        'images': images,
+        'form': form,
+        'comment_form': comment_form,
+        'users': users,
+
+    }
+    return render(request, 'index.html', params)
