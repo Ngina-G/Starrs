c106908ab1c03b991c40a50340c35f243d8631f8
Ngina <Nginagitau11@outlook.com>
2022-06-15 02:28:12 +0300

    update: adds the signup

--- awwards/urls.py
+++ awwards/urls.py
@@ -9,6 +9,13 @@ router.register('profile',views.ProfileViewSet)
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('signup/', views.signup, name='signup'),
     path('account/',include('django.contrib.auth.urls'), name='account'),
     path('api/', include(router.urls)),
+    # path('profile/<username>', views.profile, name='profile'),
+    path('api-auth/', include('rest_framework.urls',namespace='rest_framework')),
+    # path('<username>/profile',views.user_profile, name='user profile'),    
+    # path('profile/<username>/setting', views.edit_profile, name='edit'), 
+    # path('project/<post>',views.projectView,name='project'),    
+    # path('search/',views.search_project,name='search'), 
 ]
--- awwards/views.py
+++ awwards/views.py
@@ -34,6 +34,23 @@ def  index(request):
     return render(request, 'index.html', {'posts': posts,'form': form,'random_post': random_post})
 
 
+def  signup(request):
+    if request.method == 'POST':    
+        form = SignupForm(request.POST)
+        if form.is_valid(): 
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+    else: 
+        form = SignupForm
+    return render(request, 'registration/signup.html', {'form': form}) 
+
+
+    
+
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
